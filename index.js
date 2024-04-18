65ddb592849291b14f48893e42643cf49714b054
Berezovsky Alexey <lazfnol@gmail.com>
2024-04-18 14:15:40 +0300

    solution

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-// @ts-check
 
-import half from './src/half.js';
+class UserProfileManager {
+  constructor() {
+    this.users = [];
+    this.currentId = 0;
+  }
 
-export default half;
+  generateUniqueId() {
+    this.currentId = (this.currentId % 10000) + 1;
+    return this.currentId;
+  }
+
+  addUser(userInfo) {
+    userInfo.id = this.generateUniqueId();
+    this.users.push(userInfo);
+    return userInfo;
+  }
+
+  removeUser(userId) {
+    this.users = this.users.filter(user => user.id !== userId);
+  }
+
+  updateUser(userId, newInfo) {
+    this.users = this.users.map(user => {
+      if (user.id === userId) {
+        return {...user, ...newInfo};
+      }
+      return user;
+    });
+  }
+
+  findUserByName(name) {
+    return this.users.filter(user => user.name.includes(name));
+  }
+
+  getAllUsers() {
+    return this.users;
+  }
+}
+
+const profileManager = new UserProfileManager();
+
+profileManager.addUser({ name: "Alice", email: "alice@example.com" });
+profileManager.addUser({ name: "Bob", email: "bob@example.com" });
+
+console.log(profileManager.getAllUsers()); // Выводит информацию о Alice и Bob
+
+profileManager.updateUser(1, { name: "Alicia" }); // Обновляет имя Alice на Alicia
+profileManager.removeUser(2); // Удаляет Bob
+
+console.log(profileManager.getAllUsers()); // Находит Alicia
+
